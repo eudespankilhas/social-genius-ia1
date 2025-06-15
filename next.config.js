a3bd38df6c184b4fe339130a7499486ce08ddd33
eudespankilhas <153863840+eudespankilhas@users.noreply.github.com>
2025-06-15 12:32:10 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-module.exports = {
+const path = require('path');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    domains: ['localhost'],
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
   },
-}
+};
+
+module.exports = nextConfig;
